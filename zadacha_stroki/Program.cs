997b8805b0a2c202d844447b894bac3304923bc8
Serj Tigranyan <serj-tigranyan@mail.ru>
2021-12-23 23:45:27 +0200

    Добавьте файлы проекта.

---
+++ zadacha_stroki/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace zadacha_stroki
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Console.ReadLine().Replace("№", "номер по порядку"));
+        }
+    }
+}
